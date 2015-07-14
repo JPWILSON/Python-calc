f0efae81c2c2d4341008a481f7491688229c4f72
Jean-Paul Wilson <jeanpaulwilson@gmail.com>
2015-07-14 12:31:29 +0200

    Add the num_press method

--- calculator1.py
+++ calculator1.py
@@ -6,4 +6,18 @@ class Calc():
         self.eq = False
         self.op_pending = False
         self.new_no = True
+        
+    def num_press(self, num):
+        self.eq = False
+        temp = text_box.get()
+        temp2 = str(num)
+        if self.new_no:
+            self.current = temp2
+            self.new_no = False
+        else:
+            if temp2 = '.':
+                if temp2 in temp:
+                    return
+            self.current = temp + temp2
+        self.display(self.current)
     
