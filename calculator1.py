21a77f3397eb8771292dbb4a09d1713f4196eb11
Jean-Paul <jeanpaulwilson@gmail.com>
2015-07-14 11:42:19 +0200

    Add the initialising variable to the class

---
+++ calculator1.py
@@ -0,0 +1,9 @@
+class Calc():
+    def __init__(self):
+        self.total = 0
+        self.op = ""
+        self.current = ""
+        self.eq = False
+        self.op_pending = False
+        self.new_no = True
+    
